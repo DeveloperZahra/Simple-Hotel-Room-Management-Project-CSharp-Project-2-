dcd98c9d0f4eafdac2fbdf3731c70eaff725c876
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-04-07 11:20:42 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Simple_Hotel_Room_Management_Project__CSharp_Project_2__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
